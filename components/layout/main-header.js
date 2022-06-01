b3c517209710a27417dd2a5944d3fb06545a4f1b
tatsuro1997 <minagawa@dptr.jp>
2022-06-01 09:19:07 +0900

    ヘッダの修正

--- components/layout/main-header.js
+++ components/layout/main-header.js
@@ -1,20 +1,29 @@
 import Link from "next/link";
 
-import classes from "./main-header.module.css"
+import classes from "./main-header.module.css";
 
 function MainHeader() {
   return (
     <header className={classes.header}>
       <div className={classes.logo}>
-        <Link href="/">NextSpots</Link>
+        <Link href="/">バイクノリタイ</Link>
       </div>
-      <nav className={classes.navigation}>
-        <ul>
-          <li>
-            <Link href="/spots">Brwose All Spots</Link>
-          </li>
-        </ul>
-      </nav>
+      <div className={classes.navigations}>
+        <nav className={classes.navigation}>
+          <ul>
+            <li>
+              <Link href="/spots">スポット一覧</Link>
+            </li>
+          </ul>
+        </nav>
+        <nav className={classes.navigation}>
+          <ul>
+            <li>
+              <Link href="/users">ユーザー一覧</Link>
+            </li>
+          </ul>
+        </nav>
+      </div>
     </header>
   );
 }
