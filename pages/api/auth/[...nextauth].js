a26ee3f180d59e461dbfafa605c1b0e883b874cd
tatsuro1997 <minagawa@dptr.jp>
2022-06-02 11:35:08 +0900

    Adding User Registration

---
+++ pages/api/auth/[...nextauth].js
@@ -0,0 +1,42 @@
+import NextAuth from "next-auth";
+import CredentialsProviders from "next-auth/providers/credentials";
+
+import { connectDatabase } from "../../../helpers/db-util";
+import { verifyPassword } from "../../../helpers/auth-util";
+
+export default NextAuth({
+  settion: {
+    jwt: true,
+  },
+  providers: [
+    CredentialsProviders({
+      async authorize(credentials) {
+        const client = await connectDatabase();
+
+        const usersCollection = client.db().collection("users");
+
+        const user = await usersCollection.findOne({
+          email: credentials.email,
+        });
+
+        if (!user) {
+          client.close();
+          throw new Error("No user found!");
+        }
+
+        const isValid = await verifyPassword(
+          credentials.password,
+          user.password
+        );
+
+        if (!isValid) {
+          client.close();
+          throw new Error("Could not log you in!");
+        }
+
+        client.close();
+        return { email: user.email };
+      },
+    }),
+  ],
+});
---
+++ pages/api/auth/signup.js
@@ -0,0 +1,50 @@
+import { connectDatabase } from "../../../helpers/db-util";
+import { hashPassword } from "../../../helpers/auth-util";
+
+async function handler(req, res) {
+  if (req.method !== "POST") {
+    return;
+  }
+
+  const data = req.body;
+
+  const { email, password } = data;
+
+  if (
+    !email ||
+    !email.includes("@") ||
+    !password ||
+    password.trim().length < 7
+  ) {
+    res.status(422).json({
+      message:
+        "Invalid input -password should also be at least 7 characters long.",
+    });
+    return;
+  }
+
+  const client = await connectDatabase();
+
+  const db = client.db();
+
+  const existingUser = await db.collection("users").findOne({ email: email });
+
+  if (existingUser) {
+    res.status(422).json({ message: "User exists already!" });
+    client.close();
+    return;
+  }
+
+  const hashedPassword = await hashPassword(password);
+
+  const result = await db.collection("users").insertOne({
+    email: email,
+    password: hashedPassword,
+  });
+
+  res.status(201).json({
+    message: "Create user!",
+  });
+}
+
+export default handler;
