3596727a3f0428c8c4d67445fc7c5672328f7b68
tatsuro1997 <minagawa@dptr.jp>
2022-06-05 05:43:32 +0900

    プロフィールデザインの追加

--- components/profile/user-profile.js
+++ components/profile/user-profile.js
@@ -1,10 +1,13 @@
 import Image from "next/image";
 import Link from "next/link";
 
+import EditIcon from "../icons/edit-icon";
+
 import classes from "./user-profile.module.css";
 
 function UserProfile(props) {
-  const { id, name, area, prefecture, experience, bike_name, url, created_at } = props;
+  const { id, name, area, prefecture, experience, bike_name, url, created_at } =
+    props;
 
   return (
     <>
@@ -12,36 +15,54 @@ function UserProfile(props) {
         <div className={classes.card}>
           <div className={classes.profileHeader}>
             <div className={classes.profileHeader_header}>
-              <p>{created_at}登録</p>
-              <Link href={`/users/${id}/setting`}>
-                <a>setting</a>
-              </Link>
+              <p className={classes.created_at}>{created_at}登録</p>
+              <div className={classes.setting}>
+                <div className={classes.icon}>
+                  <EditIcon />
+                </div>
+                <Link href={`/users/${id}/setting`}>
+                  <a>設定</a>
+                </Link>
+              </div>
             </div>
           </div>
           <div className={classes.profileHeader_content}>
-            <div className={classes.image}>
-              <Image
-                src={"/images/no_image.webp"}
-                alt={"プロフィール画像"}
-                width={320}
-                height={200}
-              />
-            </div>
-            <p>{name}さんのプロフ</p>
+            <Image
+              src={"/images/no_image.webp"}
+              alt={"プロフィール画像"}
+              width={100}
+              height={100}
+            />
+            <div className={classes.name}>{name}さんのプロフ</div>
           </div>
-          <div className={classes.profileInfo}>
-            <p>主なツーリングエリア：{area}</p>
-            <p>都道府県：{prefecture}</p>
-            <p>バイク歴：{experience}</p>
-            <p>車種：{bike_name}</p>
-            {url && (
-              <Link href={url}>
-                <a target="_blank" rel="noopener noreferrer">
-                  URL：{url}
-                </a>
-              </Link>
-            )}
-          </div>
+          <ul className={classes.profileInfo}>
+            <li>
+              <sapn className={classes.key}>主なツーリングエリア</sapn>
+              <span className={classes.value}>{area}</span>
+            </li>
+            <li>
+              <sapn className={classes.key}>都道府県</sapn>
+              <span className={classes.value}>{prefecture}</span>
+            </li>
+            <li>
+              <sapn className={classes.key}>バイク歴</sapn>
+              <span className={classes.value}>{experience}年</span>
+            </li>
+            <li>
+              <sapn className={classes.key}>車種</sapn>
+              <span className={classes.value}>{bike_name}</span>
+            </li>
+            <li>
+              <sapn className={classes.key}>URL</sapn>
+              {url && (
+                <Link href={url}>
+                  <a target="_blank" rel="noopener noreferrer">
+                    <span className={classes.value}>{url}</span>
+                  </a>
+                </Link>
+              )}
+            </li>
+          </ul>
         </div>
       </div>
     </>
