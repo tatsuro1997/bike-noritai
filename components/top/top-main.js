2438679e117b7d921501efaa9132063e7715ff7d
tatsuro1997 <minagawa@dptr.jp>
2022-06-07 06:26:43 +0900

    TopMainの大枠作成

--- components/top/top-main.js
+++ components/top/top-main.js
@@ -7,17 +7,26 @@ import classes from "./top-main.module.css";
 function TopMain() {
   return (
     <>
-      <div className={classes.background_image}>
-        <Image src={"/images/bike1.jpg"} alt={"背景画像"} layout="fill" />
+      <div className={classes.control}>
+        <div className={classes.background_image}>
+          <Image
+            src={"/images/bike1.jpg"}
+            alt={"背景画像"}
+            layout="fill"
+            objectFit="cover"
+          />
+        </div>
+        <div className={classes.contents}>
+          <h2>今から走りに行きたいスポットが見つかる検索サイト</h2>
+          <Image
+            src={"/images/no_image.webp"}
+            alt={"ロゴ"}
+            width={550}
+            height={300}
+          />
+          <SpotsSearch />
+        </div>
       </div>
-      <h2>今から走りに行きたいスポットが見つかる検索サイト</h2>
-      <Image
-        src={"/images/no_image.webp"}
-        alt={"ロゴ"}
-        width={300}
-        height={300}
-      />
-      <SpotsSearch />
     </>
   );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,27 +1,29 @@
 import Head from "next/head";
 
-import { getFeaturedSpots } from "../helpers/spot-api-util";
+import { getAllSpots } from "../helpers/spot-api-util";
 import SpotList from "../components/spots/spot-list";
 import SpotRegistration from "../components/spots/spot-registration";
+import TopMain from "../components/top/top-main";
 
 function HomePage(props) {
   return (
     <div>
       <Head>
-        <title>NextJS Spots</title>
+        <title>バイクノリタイ</title>
         <meta
           name="description"
-          constent="Find a lot of great spots that allow you to evolve..."
+          constent="バイカー向けスポット検索サイト「バイクノリタイ」は全国〇〇箇所のスポットを掲載中。条件検索からあなたにピッタリのスポットが見つかります！"
         />
       </Head>
-      <SpotRegistration />
+      <TopMain />
+      {/* <SpotRegistration /> */}
       <SpotList items={props.spots} />
     </div>
   );
 }
 
 export async function getStaticProps() {
-  const featuredSpots = await getFeaturedSpots();
+  const featuredSpots = await getAllSpots();
 
   return {
     props: {
