389414855c670965f4116080c55eeff85910dc71
tatsuro1997 <minagawa@dptr.jp>
2022-06-17 06:27:50 +0900

    Adding Style

--- components/input/comment-list.js
+++ components/input/comment-list.js
@@ -1,18 +1,19 @@
-import classes from './comment-list.module.css';
+import classes from "./comment-list.module.css";
 
 function CommentList(props) {
   const { items } = props;
 
   return (
     <ul className={classes.comments}>
-      {items && items.map(item => (
-        <li key={item._id}>
-          <p>{item.text}</p>
-          <div>
-            By <address>{item.name}</address>
-          </div>
-        </li>
-      ))}
+      {items &&
+        items.map((item) => (
+          <li key={item._id}>
+            <p>{item.text}</p>
+            <div>
+              <address>By {item.name}</address>
+            </div>
+          </li>
+        ))}
     </ul>
   );
 }
--- components/input/new-comment.js
+++ components/input/new-comment.js
@@ -53,7 +53,7 @@ function NewComment(props) {
         <textarea id="comment" rows="5" ref={commentInputRef}></textarea>
       </div>
       {isInvalid && <p>Please enter a valid email address and comment!</p>}
-      <button>Submit</button>
+      <button>コメントする</button>
     </form>
   );
 }
