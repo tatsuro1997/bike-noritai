e4f404919639d419e998f8475f154e20ff5f603a
tatsuro1997 <minagawa@dptr.jp>
2022-06-28 07:49:47 +0900

    rankを5位まで表示に変更

--- components/spots/latest-spots.js
+++ components/spots/latest-spots.js
@@ -3,11 +3,7 @@ import SpotList from "./spot-list";
 import classes from './latest-spots.module.css';
 
 function LatestSpots({ spots }) {
-  let slicedSpots;
-
-  if (spots.length > 3) {
-    slicedSpots = spots.slice(0, 3);
-  }
+  const slicedSpots = spots.slice(0, 3);
 
   return (
     <>
