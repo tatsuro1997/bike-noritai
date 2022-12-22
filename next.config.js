753ff8abfc963db5dd67637b3a6413643671347d
tatsuro1997 <minagawa@dptr.jp>
2022-12-22 14:29:34 +0900

    エラー回避
    参考:https://github.com/vercel/next.js/discussions/42994#discussioncomment-4180165

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {
-//   reactStrictMode: true,
-// }
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  experimental: {
+    newNextLinkBehavior: false,
+  },
+};
 
-// module.exports = nextConfig
+module.exports = nextConfig
