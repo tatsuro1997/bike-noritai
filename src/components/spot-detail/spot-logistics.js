9a570690a819775dfa7bd89fe87566343d7d35e2
tatsuro1997 <minagawa@dptr.jp>
2022-12-26 12:21:56 +0900

    spot詳細の修正

--- src/components/spot-detail/spot-logistics.js
+++ src/components/spot-detail/spot-logistics.js
@@ -1,6 +1,5 @@
 import Image from "next/image";
 import Link from "next/link";
-
 import AddressIcon from "../icons/address-icon";
 import HouseIcon from "../icons/house-icon";
 import LogisticsItem from "./logistics-item";
@@ -9,9 +8,10 @@ import Map from "../map/map";
 import RecordList from "../records/record-list";
 import BookmarkAndRecordButton from "../ui/bookmark-record-button";
 
-function SpotLogistics(props) {
+const SpotLogistics = ({ spot, records }) => {
   const {
     id,
+    name,
     type,
     prefecture,
     address1,
@@ -22,83 +22,78 @@ function SpotLogistics(props) {
     off_day,
     description,
     image,
-    imageAlt,
     count,
     lat,
     lng,
-    records,
-  } = props;
-
+  } = spot;
   const exploreLink = `/spots/${id}/records`
 
   return (
-    <>
-      <section className={classes.logistics}>
-        <div className={classes.logistics_top}>
-          <div className={classes.side_content}>
-            <div className={classes.image}>
-              {image && (
-                <Image
-                  src={`/uploads/spots/${image}`}
-                  alt={imageAlt}
-                  width={320}
-                  height={200}
-                />
-              )}
-              {!image && (
-                <Image
-                  src={"/images/no_image.webp"}
-                  alt={imageAlt}
-                  width={320}
-                  height={200}
-                />
-              )}
-            </div>
-            {!lat && !lng && <p>まだ地図が登録されていません。</p>}
-            <div className={classes.map}>
-              <Map
-                lat={lat}
-                lng={lng}
-                prefecture={prefecture}
-                address1={address1}
-                address2={address2}
+    <section className={classes.logistics}>
+      <div className={classes.logistics_top}>
+        <div className={classes.side_content}>
+          <div className={classes.image}>
+            {image && (
+              <Image
+                src={`/uploads/spots/${image}`}
+                alt={name}
+                width={320}
+                height={200}
+                priority
               />
-            </div>
+            )}
+            {!image && (
+              <Image
+                src={"/images/no_image.webp"}
+                alt={name}
+                width={320}
+                height={200}
+              />
+            )}
           </div>
-          <div className={classes.main}>
-            <BookmarkAndRecordButton spotId={id} count={count} />
-            <ul className={classes.list}>
-              <LogisticsItem icon={HouseIcon}>
-                <p>{type}</p>
-              </LogisticsItem>
-              <LogisticsItem icon={AddressIcon}>
-                <address>
-                  {prefecture + " " + address1 + " " + address2}
-                </address>
-              </LogisticsItem>
-              <li>
-                <a href={hp_url} target="_blank" rel="noopener noreferrer">
-                  HP : {hp_url}
-                </a>
-              </li>
-              <li>駐車場 : {parking}</li>
-              <li>営業時間 : {open_time}</li>
-              <li>定休日 : {off_day}</li>
-              <li>{description}</li>
-            </ul>
+          {!lat && !lng && <p>まだ地図が登録されていません。</p>}
+          <div className={classes.map}>
+            <Map
+              lat={lat}
+              lng={lng}
+              prefecture={prefecture}
+              address1={address1}
+              address2={address2}
+            />
           </div>
         </div>
-
-        <div className={classes.logistics_contents}>
-          <RecordList items={records} />
-          <div className={classes.other_link}>
-            <Link href={exploreLink}>
-              <a>記録をもっとみる ＞</a>
-            </Link>
-          </div>
+        <div className={classes.main}>
+          <BookmarkAndRecordButton spotId={id} count={count} />
+          <ul className={classes.list}>
+            <LogisticsItem icon={HouseIcon}>
+              <p>{type}</p>
+            </LogisticsItem>
+            <LogisticsItem icon={AddressIcon}>
+              <address>
+                {prefecture + " " + address1 + " " + address2}
+              </address>
+            </LogisticsItem>
+            <li>
+              <a href={hp_url} target="_blank" rel="noopener noreferrer">
+                HP : {hp_url}
+              </a>
+            </li>
+            <li>駐車場 : {parking}</li>
+            <li>営業時間 : {open_time}</li>
+            <li>定休日 : {off_day}</li>
+            <li>{description}</li>
+          </ul>
         </div>
-      </section>
-    </>
+      </div>
+      <div className={classes.logistics_contents}>
+        <RecordList items={records} />
+        <div className={classes.other_link}>
+          <Link href={exploreLink}>
+            <a>記録をもっとみる ＞</a>
+          </Link>
+        </div>
+      </div>
+    </section>
   );
 }
 
--- src/pages/spots/[spotId]/index.js
+++ src/pages/spots/[spotId]/index.js
@@ -8,7 +8,6 @@ import { getThreeRecordsBySpotId } from "../../../helpers/record-api-util";
 
 function SpotDetailPage(props) {
   const spot = props.selectedSpot;
-  const spotCount = props.countedSpot;
   const comments = props.selectedComments;
   const records = props.selectedRecords;
 
@@ -29,22 +28,7 @@ function SpotDetailPage(props) {
       <SpotSummary name={spot.name} />
       <SpotLogistics
         key={spot._id}
-        id={spot._id}
-        name={spot.name}
-        type={spot.type}
-        prefecture={spot.prefecture}
-        address1={spot.address1}
-        address2={spot.address2}
-        open_time={spot.open_time}
-        off_day={spot.off_day}
-        parking={spot.parking}
-        hp_url={spot.hp_url}
-        description={spot.description}
-        image={spot.image}
-        imageAlt={spot.name}
-        count={spotCount}
-        lat={spot.lat}
-        lng={spot.lng}
+        spot={spot}
         records={records}
       />
       <Comments spotId={spot._id} comments={comments} />
